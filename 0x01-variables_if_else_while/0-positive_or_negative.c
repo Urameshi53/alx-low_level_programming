7a2ffeca7f73b39b8b1c76494dec8919cb6c097b
Urameshi53 <84080570+Urameshi53@users.noreply.github.com>
2023-03-17 23:30:17 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,7 +22,15 @@ int main(void)
 }
 
 void determine(int n){
-    if(n > 0){printf("%d is positive\n", n);}
-    else if (n == 0) {printf("%d is zero\n", n);}
-    else {printf("%d is negative\n", n);}
+    if(n > 0)
+    {
+    printf("%d is positive\n", n);
+    }
+    else if (n == 0) 
+    {
+    printf("%d is zero\n", n);
+    }
+    else {
+    printf("%d is negative\n", n);
+    }
 }
